9a215a651e9c2d54d206e764dbc32dd21b6c6f64
Clinton Carpene <clinton@carpene.id.au>
2020-12-05 13:48:37 +0800

    added flags for DNS server / port

--- tko-subs.go
+++ tko-subs.go
@@ -55,7 +55,12 @@ type Configuration struct {
 	herokuappname   *string
 	domain          *string
 	threadCount     *int
+	dnsServer       *string
+	dnsPort         *string
 }
+
+var dnsServer = "8.8.8.8"
+var dnsPort = "53"
 
 func main() {
 	config := Configuration{
@@ -68,8 +73,14 @@ func main() {
 		herokuapikey:    flag.String("herokuapikey", "", "Heroku API key"),
 		herokuappname:   flag.String("herokuappname", "", "Heroku app name"),
 		domain:          flag.String("domain", "", "Domains separated by ,"),
-		threadCount:     flag.Int("threads", 5, "Number of threads to run parallel")}
+		dnsServer:       flag.String("server", "8.8.8.8", "A DNS server to direct queries to"),
+		dnsPort:         flag.String("port", "53", "The DNS server port (you shouldn't have to change this)"),
+
+		threadCount: flag.Int("threads", 5, "Number of threads to run parallel")}
 	flag.Parse()
+	dnsServer = *config.dnsServer
+	dnsPort = *config.dnsPort
+	log.Println(dnsServer, *config.dnsServer)
 
 	cmsRecords := loadProviders(*config.recordsFilePath)
 	var allResults []DomainScan
@@ -297,7 +308,7 @@ func resolves(domain string) (bool, error) {
 	message := dns.Msg{}
 
 	message.SetQuestion(dns.Fqdn(domain), dns.TypeA)
-	r, _, err := client.Exchange(&message, "8.8.8.8:53")
+	r, _, err := client.Exchange(&message, dnsServer+":"+dnsPort)
 	if err != nil {
 		return false, err
 	}
@@ -320,7 +331,7 @@ func getCnameForDomain(domain string) (string, error) {
 	m := dns.Msg{}
 
 	m.SetQuestion(dns.Fqdn(domain), dns.TypeCNAME)
-	r, _, err := c.Exchange(&m, "8.8.8.8:53")
+	r, _, err := c.Exchange(&m, dnsServer+":"+dnsPort)
 	if err != nil {
 		return "", err
 	} else if len(r.Answer) == 0 {
@@ -335,7 +346,7 @@ func getCnameForDomain(domain string) (string, error) {
 		lastCname = record.Target
 
 		m.SetQuestion(dns.Fqdn(lastCname), dns.TypeCNAME)
-		r, _, err = c.Exchange(&m, "8.8.8.8:53")
+		r, _, err = c.Exchange(&m, dnsServer+":"+dnsPort)
 		if err != nil {
 			break
 		}
@@ -399,7 +410,7 @@ func authorityReturnRefusedOrServfail(domain string) (bool, error) {
 		return false, err
 	}
 
-	apexAuthority, err := getAuthorityForDomain(apex, "8.8.8.8")
+	apexAuthority, err := getAuthorityForDomain(apex, dnsServer)
 	if err != nil {
 		return false, err
 	}
